919e4041cb42da89536881e1fcb3a457469e660d
satya-ktirtha <satya.ktirtha@gmail.com>
2022-04-22 15:55:59 +0800

    navbar and content

---
+++ front-end/src/Navbar.js
@@ -0,0 +1,14 @@
+import './Navbar.css'
+import React from 'react';
+import Container from 'react-bootstrap/Container'
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <Container id="test">
+            </Container>
+        );
+    }
+}
+
+export default Navbar;
