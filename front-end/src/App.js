0fe81f7fbef2c04ef19f3c26718e0334f965c926
satya-ktirtha <satya.ktirtha@gmail.com>
2022-04-23 17:46:26 +0800

    login page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import './App.css';
-import Navbar from './Navbar';
-
-class Content extends React.Component {
-  render() {
-    return (
-      <div className="content">
-        <h1>content</h1>
-      </div>
-    )
-  }
-}
+import "./App.css"
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container';
+import { Routes,  Route, Link } from 'react-router-dom'
+import Login from './Login'
 
 function App() {
   return (
-    <div id="root">
-      <div className="wrapper">
-        <Navbar />
-        <Content />
-      </div>
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+            </Nav>
+            <Nav className="justify-content-end">
+              <Nav.Link as={Link} to={"/login"}>Login/Register</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path='/login' caseSensitive={false} element={<Login />} />
+      </Routes>
     </div>
   );
 }
--- front-end/src/Login.js
+++ front-end/src/Login.js
@@ -1,11 +1,61 @@
-import React from 'react';
+import "./Login.css"
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
-class Login extends React.Component {
+class LoginClass extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            user: "",
+            pass: ""
+        };
+
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onSubmit() {
+        console.log("Good");
+    }
+
+    encryptPassword() {
+
+    }
+
     render() {
         return (
-            <h1>test</h1>
+            <Container id="form">
+                <Form onSubmit={this.onSubmit}>
+                    <Form.Group className="mb-3 mt-3" controlId="formEmail">
+                        <Form.Label>Email / Username</Form.Label>
+                        <Form.Control type="email" placeholder="Enter your email or username" onChange={(e) => this.setState({user: e.target.value})} />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Enter your password" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formUserType">
+                        <Form.Label>Login as</Form.Label>
+                        <Form.Select>
+                            <option>Customer</option>
+                            <option>Airline staff</option>
+                            <option>Booking agent</option>
+                        </Form.Select>
+                    </Form.Group>
+                    <Button className="mb-3" id="btnSubmit" type="submit">
+                        Login
+                    </Button>
+                </Form>
+            </Container>
         );
     }
+}
+
+function Login() {
+    return <LoginClass />;
 }
 
 export default Login;
