6bcdcaed43a465470b426618b8e202ea3f3c85f0
satya-ktirtha <satya.ktirtha@gmail.com>
2022-04-24 23:31:41 +0700

    updated navbar and login page

--- front-end/src/Login.js
+++ front-end/src/Login.js
@@ -3,49 +3,80 @@ import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import axios from 'axios'
+import bcrypt from 'bcryptjs'
 
 class LoginClass extends React.Component {
+
+    static CUSTOMER = 1;
+    static AGENT = 2;
+    static STAFF = 3;
 
     constructor(props) {
         super(props);
 
         this.state = {
             user: "",
-            pass: ""
-        };
+            pass: "",
+            type: LoginClass.CUSTOMER
+        }
 
         this.onSubmit = this.onSubmit.bind(this);
+        this.encryptPassword = this.encryptPassword.bind(this);
     }
 
     onSubmit() {
-        console.log("Good");
+
+        axios.get('http://localhost:5000/login', {
+            params: {
+                type: this.state.type,
+                user: this.state.user,
+                password: this.state.pass 
+            }
+        }).then((res) => {
+            console.log(res['data']);
+        }).catch((err) => {
+            console.log(err.response);
+        });
     }
 
-    encryptPassword() {
-
+    encryptPassword(pass) {
+        if(pass === "")
+            return;
     }
 
     render() {
         return (
             <Container id="form">
-                <Form onSubmit={this.onSubmit}>
+                <Form>
                     <Form.Group className="mb-3 mt-3" controlId="formEmail">
                         <Form.Label>Email / Username</Form.Label>
                         <Form.Control type="email" placeholder="Enter your email or username" onChange={(e) => this.setState({user: e.target.value})} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Enter your password" />
+                        <Form.Control type="password" placeholder="Enter your password"  
+                        onChange={(e) => {
+                                this.setState({pass: e.target.value})
+                            }
+                        }/>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formUserType">
                         <Form.Label>Login as</Form.Label>
-                        <Form.Select>
+                        <Form.Select onChange={(e) => {
+                            if(e.target.value === "Airline staff")
+                                this.setState({type: LoginClass.STAFF});
+                            else if(e.target.value === "Customer") 
+                                this.setState({type: LoginClass.CUSTOMER});
+                            else if(e.target.value === "Booking agent")
+                                this.setState({type: LoginClass.AGENT});
+                        }}>
                             <option>Customer</option>
                             <option>Airline staff</option>
                             <option>Booking agent</option>
                         </Form.Select>
                     </Form.Group>
-                    <Button className="mb-3" id="btnSubmit" type="submit">
+                    <Button className="mb-3" id="btnSubmit" onMouseUp={this.onSubmit}>
                         Login
                     </Button>
                 </Form>
